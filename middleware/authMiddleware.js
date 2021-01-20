824b05e41f7e6272f241926839bae23d45b9f877
Vanessa Stewart <vstewart1213@gmail.com>
2021-01-19 17:59:48 -0800

    Add middleware to check for current user and add conditional rendering to dtermine whether name and log in buttons appear

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,22 +1,44 @@
-const jwt = require('jsonwebtoken')
+const jwt = require('jsonwebtoken');
+const User = require('../models/User');
 
-const requireAuth = (req, res, next) => {   // inside any middle ware, we get the response, request, and next objects
+const requireAuth = (req, res, next) => {
   const token = req.cookies.jwt;
 
-  // check json web token exists and is verified
+  // check json web token exists & is verified
   if (token) {
-    jwt.verify(token, 'net ninja secret', (err, decodedToken) => { // use verfiy method from jwt library, first arg is token and second is the original secret we used
+    jwt.verify(token, 'net ninja secret', (err, decodedToken) => {
       if (err) {
         console.log(err.message);
-        res.redirect('/');
+        res.redirect('/login');
       } else {
-        console.log(decodedToken)
+        console.log(decodedToken);
         next();
       }
-    })
+    });
   } else {
-    res.redirect('/');
+    res.redirect('/login');
   }
-}
+};
 
-module.exports = { requireAuth };
+// check current user
+const checkUser = (req, res, next) => {
+  const token = req.cookies.jwt;
+  if (token) {
+    jwt.verify(token, 'net ninja secret', async (err, decodedToken) => {
+      if (err) {
+        res.locals.user = null;
+        next();
+      } else {
+        let user = await User.findById(decodedToken.id);
+        res.locals.user = user;
+        next();
+      }
+    });
+  } else {
+    res.locals.user = null;
+    next();
+  }
+};
+
+
+module.exports = { requireAuth, checkUser };
